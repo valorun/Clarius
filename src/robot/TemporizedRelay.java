125b6c97bc00bc5ebaea86df86c8f108a9e72d05
valorun <ronan@collier-famille.fr>
2017-11-30 09:02:20 +0100

    Update TemporizedRelay.java

--- src/robot/TemporizedRelay.java
+++ src/robot/TemporizedRelay.java
@@ -4,7 +4,9 @@ public class TemporizedRelay extends Relay{
 	int tempo;
 	/**
      * Constructeur de la classe Relay
+     * @param name représente le nom de l'action du relai correspondant au relay.
      * @param pin représente le numéro du pin correspondant au relay.
+     * @param tempo représente la temporisation la temporisation pour executer un pas de l'action du relay.
      */
 	public TemporizedRelay(String name, String pin, int tempo){
 		super(name, pin);
@@ -18,16 +20,25 @@ public class TemporizedRelay extends Relay{
 	{
 		super.enableRelayFor(tempo);
 	}
-
+	/**
+	 * Méthode permettant d'obtenir la temporisation associée au relai.
+	 * @return la temporisation associé au relai.
+	 */
 	public int getTempo() {
 		return tempo;
 	}
-
+	/**
+	 * Méthode permettant de modifier la temporisation associée au relai.
+	 * @param tempo la nouvelle temporisation associé au relai.
+	 */
 	public void setTempo(int tempo) {
 		this.tempo = tempo;
 	}
 
-
+	/**
+	 * Méthode permettant d'obtenir les informations du relai sous forme de String.
+	 * @return le nom du relai et sa temporisation associé.
+	 */
 	public String toString(){
 		return name+";"+tempo;
 	}
