b26a7d2aec50f8a170fd26f2e088ffa04d26575b
nanolauryn <42538591+nanolauryn@users.noreply.github.com>
2018-08-20 23:51:34 +0200

    Update Sequence.java

--- Clarius/src/robot/Sequence.java
+++ Clarius/src/robot/Sequence.java
@@ -3,7 +3,7 @@ package robot;
 import java.util.ArrayList;
 
 /**
-* Classe permettant d'instancier une séquence de pins a activer et désactiver successivement.
+* Classe permettant d'instancier une séquence de pins à activer et désactiver successivement.
 * @author Ronan COLLIER
 * @version 1.0
 */
@@ -21,7 +21,7 @@ public class Sequence{
 	}
 	/**
 	 * Méthode permettant d'ajouter relai à la séquence.
-	 * @param relay représente le pin du relay a activer.
+	 * @param relay représente le pin du relay à activer.
 	 * @param time représente le temps en millisecondes, pendant lequel on active le relay.
 	 */
 	public void addRelay(String relay, int time) {
@@ -30,7 +30,7 @@ public class Sequence{
 	}
 	/**
 	 * Méthode permettant d'ajouter relai à la séquence.
-	 * @param relay représente le relay a activer.
+	 * @param relay représente le relay à activer.
 	 * @param time représente le temps en millisecondes, pendant lequel on active le relay.
 	 */
 	public void addRelay(Relay relay, int time) {
@@ -45,7 +45,7 @@ public class Sequence{
 		return timeTotal;
 	}
 	/**
-	 * Méthode permettant d'éxécuter la séquence.
+	 * Méthode permettant d'exécuter la séquence.
 	 */
 	public void execute() {
 		String command= "/home/pi/Scripts/sequence.bash";
